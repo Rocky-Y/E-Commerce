10783ccd596cc8cc75776f7fdd6a4c9d22b407c7
Rocky-Y <44081270+Rocky-Y@users.noreply.github.com>
2019-01-11 21:28:17 +0800

    Add files via upload

---
+++ 1688自动下载图片/1688_1.2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : Rocky-Y
+# @Email   : 1347634801@qq.com
+
+"""
+
+
+"""
+
+import re
+import json
+import requests
+from click._compat import raw_input
+import time
+from requests.cookies import RequestsCookieJar
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from lxml import etree
+
+# 获取图片url
+def get_image_url(response):
+	pass
+
+
+# 持久化
+def get_image(name,url):
+    pass
+
+# 扫码登录
+def login(url):
+    pass
+
+
+# 检查cookies是否有效，是否需要重新登录
+def check_login(url,cookie):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+	url_list = []
+	main()
+
+
+
